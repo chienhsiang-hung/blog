1cfc88a073b746bb43698255e3d57328824b669f
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2022-11-01 18:50:57 +0800

    Update Simple Topological Sorting (DAG).py

--- content/posts/2022/topological sorting/Simple Topological Sorting (DAG).py
+++ content/posts/2022/topological sorting/Simple Topological Sorting (DAG).py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 
+
 class Graph:
     def __init__(self, vertices):
         '''
@@ -8,16 +9,39 @@ class Graph:
         self.graph = defaultdict(list) # adjacency list
         self.V = vertices
     
+
     def addEdge(self, u, v):
         self.graph[u].append(v)
+
 
     def topologicalSort(self):
         visited = [False] * self.V
         stack = []
 
-        for edge in range(self.V):
-            pass
+        for i in range(self.V):
+            if not visited[i]:
+                self.topologicalUtil(i, visited, stack)
+
+        return stack
     
-    def topologicalUtil(self, visited):
-        visited[]
-            
+
+    def topologicalUtil(self, i, visited, stack):
+        visited[i] = True
+
+        for j in self.graph[i]:
+            if not visited:
+                self.topologicalUtil(j, visited, stack)
+        
+        stack.append(i)
+
+
+if __name__ == '__main__':
+    g = Graph(6)
+    g.addEdge(5, 2)
+    g.addEdge(5, 0)
+    g.addEdge(4, 0)
+    g.addEdge(4, 1)
+    g.addEdge(2, 3)
+    g.addEdge(3, 1)
+
+    print(g.topologicalSort())
